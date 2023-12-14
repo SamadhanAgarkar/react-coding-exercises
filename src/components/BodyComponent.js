9f8132386052f2cbf2bb53a26903f303fc91bb8b
unknown <sagarkar088@gmail.com>
2023-12-14 23:27:20 +0530

    added clear filter functionality

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,27 +1,41 @@
-import CartComponent from "./CartComponent"
-import {resList} from "../utils/mockData"
+import CartComponent from "./CartComponent";
+import { resList } from "../utils/mockData";
 import { useState } from "react";
 
 const BodyComponent = () => {
-    const[restaurantList, setRestaurantList] = useState(resList)
+  const [restaurantList, setRestaurantList] = useState(resList);
 
-    return (
-        <div>
-            <div className="search">
-                <button className="btn" onClick={()=>{
-                    const filterList = resList.filter(
-                        (res) => res.data.avgRating > 4
-                    );
-                    setRestaurantList(filterList);
-                }}>Filter</button>
-            </div>
-            <div className="cart-data">
-                {restaurantList.map((restaurant) => (
-                    <CartComponent key={restaurant.id} resData={restaurant} ></CartComponent>
-                ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="search">
+        <button
+          className="btn"
+          onClick={() => {
+            setRestaurantList(resList);
+          }}
+        >
+          Clear Filter
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            const filterList = resList.filter((res) => res.data.avgRating > 4);
+            setRestaurantList(filterList);
+          }}
+        >
+          Filter on Rating
+        </button>
+      </div>
+      <div className="cart-data">
+        {restaurantList.map((restaurant) => (
+          <CartComponent
+            key={restaurant.id}
+            resData={restaurant}
+          ></CartComponent>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default BodyComponent;
